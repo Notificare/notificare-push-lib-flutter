d0f996f14d573a00f9daf7621476b81da6f70a6c
Helder Pinhal <helder@notifica.re>
2020-04-02 14:52:07 +0100

    Fix cloud operation null support
    fix: check for null parameters on doCloudHostOperation to prevent serialisation errors of null objects

--- android/src/main/java/re/notifica/flutter/NotificarePushLibPlugin.java
+++ android/src/main/java/re/notifica/flutter/NotificarePushLibPlugin.java
@@ -842,9 +842,9 @@ public class NotificarePushLibPlugin implements FlutterPlugin, ActivityAware, Ap
       }
     } else if ("doCloudHostOperation".equals(call.method)) {
       if (call.hasArgument("verb") && call.hasArgument("path") && call.argument("verb") != null && call.argument("path") != null) {
-        JSONObject body = call.argument("body");
-        JSONObject params = call.argument("params");
-        JSONObject headers = call.argument("headers");
+        JSONObject body = call.hasArgument("body") ? call.argument("body") : null;
+        JSONObject params = call.hasArgument("params") ? call.argument("params") : null;
+        JSONObject headers = call.hasArgument("params") ? call.argument("headers") : null;
 
         Map<String, String> paramsMap = new HashMap<>();
         if (params != null) {
