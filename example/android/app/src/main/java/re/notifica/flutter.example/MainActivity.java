3148b64584d0a9e19762e2cf74a3fb3cae6ef96d
Helder Pinhal <helder@notifica.re>
2020-06-18 11:16:43 +0100

    [Example] Fix Flutter v1.17 plugin registration

--- example/android/app/src/main/java/re/notifica/flutter.example/MainActivity.java
+++ example/android/app/src/main/java/re/notifica/flutter.example/MainActivity.java
@@ -1,12 +1,7 @@
 package re.notifica.flutter.example;
 
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(FlutterEngine flutterEngine) {
-//    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+
 }
